cea68f7fd3eb9fcfb67606c061b9575c66d0ff28
joshuapeake <joshuapeake@gmail.com>
2026-04-25 17:03:46 -0500

    Create tailwind.config.js with Tailwind CSS configuration for Next.js project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'pages/**/*.{js,ts,jsx,ts}',
+    'components/**/*.{js,ts,jsx,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
